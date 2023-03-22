5a5bf54f81c68fcbe2daad04bb4825bd11c3ed46
ijyu <ijyu93@gmail.com>
2023-03-22 17:02:50 +0900

    chore: schema & yml 설정
    기본 테이블 생성을 위한 schema.sql 추가
    JPA 사용시 필요한 설정 및 h2 관련 설정 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,29 @@
+drop table if exists member cascade;
+
+create table member (
+--     id int unsigned auto_increment,
+    id int auto_increment,
+    name varchar(255) not null,
+--     age int unsigned not null default 0,
+    age int not null default 0,
+    city varchar(255),
+    street varchar(255),
+    zip_code varchar(18),
+    created_date timestamp,
+    last_modified_date timestamp,
+    primary key (id)
+);
+
+drop table if exists pet cascade;
+
+create table pet (
+--     id int unsigned auto_increment,
+    id int auto_increment,
+    name varchar(255) not null,
+    species varchar(255) not null,
+--     age int unsigned not null default 0,
+    age int not null default 0,
+    user_id int not null,
+    primary key (id),
+    foreign key (user_id) references member(id)
+)
