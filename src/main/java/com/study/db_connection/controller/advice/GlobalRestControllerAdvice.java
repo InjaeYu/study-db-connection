f7f12781e9b3c5223e4aede88af6931e305a7fa1
ijyu <ijyu93@gmail.com>
2023-03-29 17:57:25 +0900

    fix: HttpMessageNotReadableException 글로벌 예외 처리 추가
    Request body의 변환 과정에서 예외 발생시 ErrorDto로 반환하기 위해 추가
    각 예외 핸들러에 예외 메시지 로깅 추가
    ErrorDto 생성 메서드 추가

--- src/main/java/com/study/db_connection/controller/advice/GlobalRestControllerAdvice.java
+++ src/main/java/com/study/db_connection/controller/advice/GlobalRestControllerAdvice.java
@@ -4,28 +4,43 @@ import com.study.db_connection.controller.dto.ErrorDto;
 import com.study.db_connection.controller.dto.ResponseDto;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.NoSuchElementException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalRestControllerAdvice {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseDto noSuchElementExceptionHandler(HttpServletRequest request, NoSuchElementException e) {
-        ErrorDto errorDto = new ErrorDto(HttpStatus.NOT_FOUND.toString(), e.getMessage(),
-            request.getRequestURI());
-        return new ResponseDto(errorDto);
+        log.info(e.getMessage());
+        return new ResponseDto(getErrorDto(HttpStatus.NOT_FOUND, e, request));
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class})
     public ResponseDto badRequestExceptionHandler(HttpServletRequest request, RuntimeException e) {
-        ErrorDto errorDto = new ErrorDto(HttpStatus.BAD_REQUEST.toString(), e.getMessage(),
-            request.getRequestURI());
-        return new ResponseDto(errorDto);
+        log.info(e.getMessage());
+        return new ResponseDto(getErrorDto(HttpStatus.BAD_REQUEST, e, request));
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseDto httpMessageNotReadableExceptionHandler(HttpServletRequest request, HttpMessageNotReadableException e) {
+        log.info(e.getMessage());
+        return new ResponseDto(getErrorDto(HttpStatus.BAD_REQUEST, "Unknown request body", request));
+    }
+
+    private ErrorDto getErrorDto(HttpStatus status, Exception e, HttpServletRequest req) {
+        return new ErrorDto(status.toString(), e.getMessage(), req.getRequestURI());
+    }
+
+    private ErrorDto getErrorDto(HttpStatus status, String message, HttpServletRequest req) {
+        return new ErrorDto(status.toString(), message, req.getRequestURI());
+    }
 }
