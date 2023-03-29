c283af1c423c5410e9d2899ae92023681ddc7f9b
ijyu <ijyu93@gmail.com>
2023-03-29 18:00:05 +0900

    feat: spring data jpa controller 테스트 로직 추가
    findAll, findById, delete, update 로직에 대한 테스트 추가
    MemberUpdateDto 생성자 추가 (테스트 용도)

--- src/main/java/com/study/db_connection/controller/dto/MemberUpdateDto.java
+++ src/main/java/com/study/db_connection/controller/dto/MemberUpdateDto.java
@@ -7,8 +7,16 @@ public class MemberUpdateDto {
 
     //TODO : Validation 추가 필요
     private String name;
-    private Integer age;
+    private int age;
     private String city;
     private String street;
     private String zipCode;
+
+    public MemberUpdateDto(String name, int age, String city, String street, String zipCode) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+        this.street = street;
+        this.zipCode = zipCode;
+    }
 }
--- src/test/java/com/study/db_connection/controller/springdatajpa/SpringDataJpaMemberControllerTest.java
+++ src/test/java/com/study/db_connection/controller/springdatajpa/SpringDataJpaMemberControllerTest.java
@@ -1,34 +1,42 @@
 package com.study.db_connection.controller.springdatajpa;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.study.db_connection.controller.dto.MemberSaveDto;
+import com.study.db_connection.controller.dto.MemberUpdateDto;
+import com.study.db_connection.entity.Address;
 import com.study.db_connection.entity.Member;
 import com.study.db_connection.service.springdatajpa.SpringDataJpaMemberService;
+import java.util.NoSuchElementException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @WebMvcTest(SpringDataJpaMemberController.class)
 class SpringDataJpaMemberControllerTest {
 
     @Autowired
     MockMvc mockMvc;
-
+    @Autowired
+    ObjectMapper mapper;
     @MockBean
     SpringDataJpaMemberService service;
-
-    ObjectMapper mapper = new ObjectMapper();
 
     @Test
     void save() throws Exception {
@@ -57,6 +65,7 @@ class SpringDataJpaMemberControllerTest {
 
         //then
         actions.andExpect(status().isCreated())
+            .andExpect(jsonPath("$.data").isNotEmpty())
             .andExpect(jsonPath("$.data.id").value(memberId))
             .andExpect(jsonPath("$.data.name").value(name))
             .andExpect(jsonPath("$.data.age").value(age))
@@ -66,43 +75,176 @@ class SpringDataJpaMemberControllerTest {
     }
 
     @Test
-    void findAll() {
+    void findAll() throws Exception {
         //given
 
         //when
+        ResultActions actions = mockMvc.perform(
+            get("/spring-data-jpa/members")
+                .accept(MediaType.APPLICATION_JSON)
+        );
 
         //then
-
+        actions.andExpect(status().isOk())
+            .andExpect(jsonPath("$.data").isArray())
+            .andExpect(jsonPath("$.data").isEmpty());
     }
 
     @Test
-    void findById() {
+    void findById() throws Exception {
         //given
+        String name = "memberA";
+        int age = 10;
+        String city = "newCity";
+        String street = "newStreet";
+        String zipCode = "012-3324";
+        given(service.findById(anyLong())).willAnswer(invocation -> {
+            Long id = invocation.getArgument(0);
+            Member member = getMember(name, age, city, street, zipCode);
+            ReflectionTestUtils.setField(member, "id", id);
+            return member;
+        });
+        Long memberId = 10L;
 
         //when
+        ResultActions actions = mockMvc.perform(
+            get("/spring-data-jpa/members/{id}", memberId)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+        );
 
         //then
-
+        actions.andExpect(status().isOk())
+            .andExpect(jsonPath("$.data").isNotEmpty())
+            .andExpect(jsonPath("$.data.id").value(memberId))
+            .andExpect(jsonPath("$.data.name").value(name))
+            .andExpect(jsonPath("$.data.age").value(age))
+            .andExpect(jsonPath("$.data.city").value(city))
+            .andExpect(jsonPath("$.data.street").value(street))
+            .andExpect(jsonPath("$.data.zipCode").value(zipCode));
     }
 
     @Test
-    void delete() {
+    void findById_존재하지_않는_유저() throws Exception {
         //given
+        Long memberId = 10L;
+        given(service.findById(anyLong())).willThrow(
+            new NoSuchElementException("Member not found(id : " + memberId + ")"));
 
         //when
+        ResultActions actions = mockMvc.perform(
+            get("/spring-data-jpa/members/{id}", memberId)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+        );
 
         //then
-
+        actions.andExpect(status().isNotFound())
+            .andExpect(jsonPath("$.data").isNotEmpty())
+            .andExpect(jsonPath("$.data.code").value(HttpStatus.NOT_FOUND.toString()))
+            .andExpect(jsonPath("$.data.message").value("Member not found(id : " + memberId + ")"))
+            .andExpect(jsonPath("$.data.url").value("/spring-data-jpa/members/" + memberId));
     }
 
     @Test
-    void update() {
+    void delete() throws Exception {
         //given
+        Long memberId = 10L;
 
         //when
+        ResultActions actions = mockMvc.perform(
+            MockMvcRequestBuilders.delete("/spring-data-jpa/members/{id}", memberId)
+        );
 
         //then
+        actions.andExpect(status().isNoContent());
+    }
 
+    @Test
+    void delete_존재하지_않는_유저() throws Exception {
+        //given
+        Long memberId = 10L;
+        willThrow(new NoSuchElementException("Member not found(id : " + memberId + ")")).given(
+            service).delete(anyLong());
+
+        //when
+        ResultActions actions = mockMvc.perform(
+            MockMvcRequestBuilders.delete("/spring-data-jpa/members/{id}", memberId)
+        );
+
+        //then
+        actions.andExpect(status().isNotFound())
+            .andExpect(jsonPath("$.data").isNotEmpty())
+            .andExpect(jsonPath("$.data.code").value(HttpStatus.NOT_FOUND.toString()))
+            .andExpect(jsonPath("$.data.message").value("Member not found(id : " + memberId + ")"))
+            .andExpect(jsonPath("$.data.url").value("/spring-data-jpa/members/" + memberId));
+    }
+
+    @Test
+    void update() throws Exception {
+        //given
+        given(service.update(anyLong(), any(Member.class))).willAnswer(invocation -> {
+            Long id = invocation.getArgument(0);
+            Member member = invocation.getArgument(1);
+            ReflectionTestUtils.setField(member, "id", id);
+            return member;
+        });
+        Long memberId = 10L;
+        String name = "name";
+        int age = 10;
+        String city = "city", street = "street", zipCode = "000-0000";
+        MemberUpdateDto dto = new MemberUpdateDto(name, age, city, street, zipCode);
+        String content = mapper.writeValueAsString(dto);
+
+        //when
+        ResultActions actions = mockMvc.perform(
+            patch("/spring-data-jpa/members/{id}", memberId)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content)
+        );
+
+        //then
+        actions.andExpect(status().isOk())
+            .andExpect(jsonPath("$.data.id").value(memberId))
+            .andExpect(jsonPath("$.data.name").value(name))
+            .andExpect(jsonPath("$.data.age").value(age))
+            .andExpect(jsonPath("$.data.city").value(city))
+            .andExpect(jsonPath("$.data.street").value(street))
+            .andExpect(jsonPath("$.data.zipCode").value(zipCode));
+    }
+
+    @Test
+    void update_존재하지_않는_멤버() throws Exception {
+        //given
+        Long memberId = 10L;
+        given(service.update(anyLong(), any(Member.class))).willThrow(
+            new NoSuchElementException("Member not found(id : " + memberId + ")"));
+        String name = "name";
+        int age = 10;
+        String city = "city", street = "street", zipCode = "000-0000";
+        MemberUpdateDto dto = new MemberUpdateDto(name, age, city, street, zipCode);
+        String content = mapper.writeValueAsString(dto);
+
+        //when
+        ResultActions actions = mockMvc.perform(
+            patch("/spring-data-jpa/members/{id}", memberId)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content)
+        );
+
+        //then
+        actions.andExpect(status().isNotFound())
+            .andExpect(jsonPath("$.data").isNotEmpty())
+            .andExpect(jsonPath("$.data.code").value(HttpStatus.NOT_FOUND.toString()))
+            .andExpect(jsonPath("$.data.message").value("Member not found(id : " + memberId + ")"))
+            .andExpect(jsonPath("$.data.url").value("/spring-data-jpa/members/" + memberId));
+    }
+
+    private Member getMember(String name, int age, String city, String street, String zipCode) {
+        Address address = new Address(city, street, zipCode);
+        return new Member(name, age, address);
     }
 
 }
