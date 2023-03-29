10659c10a1f2f58f5ff4918ac096475da900ef10
ijyu <ijyu93@gmail.com>
2023-03-29 16:01:12 +0900

    refactor: jpa auditing configure 분리
    @WebMvcTest를 활용한 테스트시 jpa auditing 관련 bean을 찾으며 문제 발생
    따라서 jpa auditing 설정을 따로 하도록 변경

--- src/main/java/com/study/db_connection/DbConnectionApplication.java
+++ src/main/java/com/study/db_connection/DbConnectionApplication.java
@@ -2,14 +2,12 @@ package com.study.db_connection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class DbConnectionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DbConnectionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DbConnectionApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/study/db_connection/config/JpaAuditingConfiguration.java
@@ -0,0 +1,10 @@
+package com.study.db_connection.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+
+}
